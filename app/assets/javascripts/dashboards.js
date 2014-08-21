23cf93501687d6d1ea4c4b52567efa15fefbd88b
Matthew Powers <matthewkevinpowers@gmail.com>
2014-08-21 08:16:43 -0400

    Removed Google Maps Rails gem and got a marker placement up and running.

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,41 +1,58 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  var handler = Gmaps.build('Google');
+  var mapOptions = {
+    zoom: 11
+  };
 
-  handler.buildMap({ provider: {zoom: 14}, internal: {id: 'geolocation'} }, function(){
-    if(navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(displayOnMap);
-  });
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  function displayOnMap(position){
-    var marker = handler.addMarker({
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
     });
-
-    handler.map.centerOn(marker);
-
-    google.maps.event.addListener(handler.getMap(), 'click', function(event) {
-      var lat = event.latLng.lat();
-      var lng = event.latLng.lng();
-      $.ajax({
-        type: "POST",
-        url: "dashboards/instagram_search",
-        data: ({latitude: lat, longitude: lng}),
-        dataType: "json",
-        success: function (data) {
-          $.each(data, function(i, item) {
-            console.log(item.location.latitude + " " + item.location.longitude + " " + item.user.username);
-          });
-        }
-      });
-    });
-  }; // end displayOnMap()
+  } else {
+    var australia = new google.maps.LatLng(-34.397, 150.644);
+    map.setCenter(australia);
+  };
 
   $.each(gon.instagram_search, function(i, item) {
-      console.log(item);
-      var html = "<tr><td>" + item.user.full_name + "</td><td>" + "<img src=\"" + item.user.profile_picture + "\"/>" + "</td><td>" + "<img src=\"" + item.images.thumbnail.url + "\"/>" + "</td>" + "<td>" + item.likes.count + "</td><td>" + item.user.username + "</td></tr>";
-      $('.table').append(html);
+    var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+    new google.maps.Marker({
+      position: location,
+      map: map,
+      title: "blah"
+    });
+  });
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+    $.ajax({
+      type: "POST",
+      url: "dashboards/instagram_search",
+      data: ({latitude: lat, longitude: lng}),
+      dataType: "json",
+      success: function (data) {
+        map.setCenter(event.latLng)
+        $.each(data, function(i, item) {
+          var infowindow = new google.maps.InfoWindow({
+            content: "<img src=" + item.images.low_resolution.url + ">"
+          });
+          console.log(item);
+          var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+          var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            title: "blah"
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+          });
+        });
+      }
+    });
   });
 
 });
+
