f3b630775d81a0d110f355f56cc62304195e3a9a
Matthew Powers <matthewkevinpowers@gmail.com>
2014-08-21 22:02:14 -0400

    Added images in the info windows

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -70,12 +70,18 @@ $(document).ready(function() {
       success: function (data) {
         map.setCenter(event.latLng)
         $.each(data, function(i, item) {
+          var infowindow = new google.maps.InfoWindow({
+            content: "<img src=" + item.images.low_resolution.url + ">"
+          });
           console.log(item);
           var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
-          new google.maps.Marker({
+          var marker = new google.maps.Marker({
             position: location,
             map: map,
             title: "blah"
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
           });
         });
       }
