9f94391f8366199c3d408127dbd17035211ee647
Matthew Powers <matthewkevinpowers@gmail.com>
2014-08-25 08:48:33 -0400

    minor cleanup

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,39 +1,5 @@
 $(document).ready(function() {
 
-  //var handler = Gmaps.build('Google');
-
-  //handler.buildMap({ provider: {zoom: 14}, internal: {id: 'geolocation'} }, function(){
-    //if(navigator.geolocation)
-      //navigator.geolocation.getCurrentPosition(displayOnMap);
-  //});
-
-  //function displayOnMap(position){
-    //var marker = handler.addMarker({
-      //lat: position.coords.latitude,
-      //lng: position.coords.longitude
-    //});
-
-    //handler.map.centerOn(marker);
-
-    //google.maps.event.addListener(handler.getMap(), 'click', function(event) {
-      //var lat = event.latLng.lat();
-      //var lng = event.latLng.lng();
-      //$.ajax({
-        //type: "POST",
-        //url: "dashboards/instagram_search",
-        //data: ({latitude: lat, longitude: lng}),
-        //dataType: "json",
-        //success: function (data) {
-          //setCenter(event.latLng)
-          //$.each(data, function(i, item) {
-            //console.log(item.location.latitude + " " + item.location.longitude + " " + item.user.username);
-          //});
-        //}
-      //});
-    //});
-
-  //}; // end displayOnMap()
-
   var mapOptions = {
     zoom: 11
   };
