463b0d8c363a206bf0984d3782009261f286fcb6
Matthew Powers <matthewkevinpowers@gmail.com>
2014-08-28 22:15:30 -0400

    Refactor code to make it cleaner

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,59 +1,72 @@
 $(document).ready(function() {
+  google.maps.event.addDomListener(window, 'load', function() {
+    var mapOptions = {
+      zoom: 11
+    };
 
-  var mapOptions = {
-    zoom: 11
-  };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        getPictures(latLng);
+      });
+    } else {
+      var australia = new google.maps.LatLng(-34.397, 150.644);
+      map.setCenter(australia);
+    };
 
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      getPictures(latLng);
+    google.maps.event.addListener(map, 'click', function(event) {
+      getPictures(event.latLng);
     });
-  } else {
-    var australia = new google.maps.LatLng(-34.397, 150.644);
-    map.setCenter(australia);
-  };
 
-  google.maps.event.addListener(map, 'click', function(event) {
-    getPictures(event.latLng);
-  });
+    function dropPins (latLng, data) {
+      map.setCenter(latLng);
+      map.setZoom(13);
+      $.each(data, function(i, item) {
+        var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+        var marker = createMarker(location);
+        var infowindow = createInfoWindow(item);
+        addMarkerListener(marker, infowindow);
+      });
+    }
 
-  function dropPins (latLng, data) {
-    map.setCenter(latLng);
-    map.setZoom(13);
-    $.each(data, function(i, item) {
-      var infowindow = new google.maps.InfoWindow({
-        content: "<img src=" + item.images.low_resolution.url + ">"
-      });
-      var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        title: "blah"
-      });
+    function addMarkerListener(marker, infowindow) {
       google.maps.event.addListener(marker, 'click', function() {
         infowindow.open(map, marker);
       });
-    });
-  }
+    }
 
-  function getPictures (latLng) {
-    $.ajax({
-      type: "POST",
-      url: "dashboards/instagram_search",
-      data: instagramFormattedLatLng(latLng),
-      dataType: "json",
-      success: function (data) {
-        dropPins(latLng, data);
-      }
-    });
-  }
+    function createInfoWindow (item) {
+      return new google.maps.InfoWindow({
+        content: "<img src=" + item.images.low_resolution.url + ">"
+      });
+    }
 
-  function instagramFormattedLatLng (latLng) {
-    return {latitude: latLng.lat(), longitude: latLng.lng()}
-  }
+    function createMarker (latLng) {
+      return new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: "blah"
+      });
+    }
 
+    function getPictures (latLng) {
+      $.ajax({
+        type: "POST",
+        url: "dashboards/instagram_search",
+        data: instagramFormattedLatLng(latLng),
+        dataType: "json",
+        success: function (data) {
+          dropPins(latLng, data);
+        }
+      });
+    }
+
+    function instagramFormattedLatLng (latLng) {
+      return {latitude: latLng.lat(), longitude: latLng.lng()}
+    }
+
+  });
 });
 
