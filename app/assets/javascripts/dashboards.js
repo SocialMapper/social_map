cbc023f797c90949cf470a7a713bc236b5fc4ec8
Matthew Powers <matthewkevinpowers@gmail.com>
2014-08-28 21:35:15 -0400

    Refactor JavaScript code to methods so it's more readable

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
       map.setCenter(initialLocation);
     });
   } else {
@@ -17,34 +17,43 @@ $(document).ready(function() {
   };
 
   google.maps.event.addListener(map, 'click', function(event) {
-    var lat = event.latLng.lat();
-    var lng = event.latLng.lng();
+    getPictures(event);
+  });
+
+  function dropPins (latLng, data) {
+    map.setCenter(latLng);
+    map.setZoom(13);
+    $.each(data, function(i, item) {
+      var infowindow = new google.maps.InfoWindow({
+        content: "<img src=" + item.images.low_resolution.url + ">"
+      });
+      var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: "blah"
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+      });
+    });
+  }
+
+  function getPictures (event) {
     $.ajax({
       type: "POST",
       url: "dashboards/instagram_search",
-      data: ({latitude: lat, longitude: lng}),
+      data: instagramFormattedLatLng(event.latLng),
       dataType: "json",
       success: function (data) {
-        map.setCenter(event.latLng);
-        map.setZoom(13);
-        $.each(data, function(i, item) {
-          var infowindow = new google.maps.InfoWindow({
-            content: "<img src=" + item.images.low_resolution.url + ">"
-          });
-          console.log(item);
-          var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
-          var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            title: "blah"
-          });
-          google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-          });
-        });
+        dropPins(event.latLng, data);
       }
     });
-  });
+  }
+
+  function instagramFormattedLatLng (latLng) {
+    return {latitude: latLng.lat(), longitude: latLng.lng()}
+  }
 
 });
 
