d6acd1b177abdae1f4567350d2cc76cb34cfaae3
Matthew Powers <matthewkevinpowers@gmail.com>
2014-09-09 09:33:00 -0400

    Refactor JavaScript to be more object oriented

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,71 +1,71 @@
 $(document).ready(function() {
-  google.maps.event.addDomListener(window, 'load', function() {
-    var googleMap = {
-      options: {zoom: 13},
-      map: function () {
-        return new google.maps.Map(document.getElementById("map-canvas"), this.options);
-      },
-      clickListener: function () {
-        google.maps.event.addListener(map, 'click', function(event) {
-          getPictures(event.latLng);
+
+  // google maps stuff
+  var googleMap = {
+    options: {zoom: 13},
+    map: function () {
+      return new google.maps.Map(document.getElementById("map-canvas"), this.options);
+    },
+    clickListener: function () {
+      google.maps.event.addListener(map, 'click', function(event) {
+        getPictures(event.latLng);
+      });
+    },
+    setInitialLocation: function () {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          getPictures(latLng);
         });
-      },
-      setInitialLocation: function () {
-        if(navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            getPictures(latLng);
-          });
-        } else {
-          var australia = new google.maps.LatLng(-34.397, 150.644);
-          map.setCenter(australia);
-        };
-      }
+      } else {
+        var australia = new google.maps.LatLng(-34.397, 150.644);
+        map.setCenter(australia);
+      };
+    },
+    addMarkerListener: function (marker, item) {
+      google.maps.event.addListener(marker, 'click', function() {
+        map.panTo(marker.getPosition());
+        $.fancybox({
+          content: fancyboxContent(item),
+          title: captionText(item)
+        });
+        Instagram.getComments(item.id);
+      });
     }
+  }
 
-    var map = googleMap.map();
-    googleMap.clickListener();
-    googleMap.setInitialLocation();
+  var map = googleMap.map();
+  googleMap.clickListener();
+  googleMap.setInitialLocation();
 
-    var markers = [];
 
-    var input = (document.getElementById('pac-input'));
+  // google maps search box
+  var googleSearch = {
+    input: (document.getElementById('pac-input')),
+    controls: function () {
       map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    },
+    searchBox: function () {
+      return new google.maps.places.SearchBox((this.input));
+    },
+    addListener: function () {
+      google.maps.event.addListener(this.searchBox, 'places_changed', function() {
+        var places = this.searchBox.getPlaces();
+        var place = places[0];
+        getPictures(place.geometry.location);
+      });
+    }
+  }
 
-    var searchBox = new google.maps.places.SearchBox((input));
-
-    google.maps.event.addListener(searchBox, 'places_changed', function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
-        return;
-      }
-      for (var i = 0, marker; marker = markers[i]; i++) {
-        marker.setMap(null);
-      }
-
-      markers = [];
-      var bounds = new google.maps.LatLngBounds();
-      for (var i = 0, place; place = places[i]; i++) {
-        var image = {
-          url: place.icon,
-          size: new google.maps.Size(0, 0)
-        };
-        var marker = new google.maps.Marker({
-          map: map,
-          icon: image,
-          position: place.geometry.location
-        });
-        markers.push(marker);
-        bounds.extend(place.geometry.location);
-      }
-      map.fitBounds(bounds);
-      map.setZoom(13);
-      getPictures(marker.position);
-    });
+  var input = googleSearch.input;
+  googleSearch.controls();
+  var searchBox = googleSearch.searchBox();
+  googleSearch.addListener();
 
 
-    function getComments (instagramId) {
+  // instagram related stuff
+  var Instagram = {
+    getComments: function (instagramId) {
       $.ajax({
         type: "POST",
         url: "dashboards/instagram_comments",
@@ -76,81 +76,70 @@ $(document).ready(function() {
         }
       });
     }
+  }
 
-    function addCommentsToFancybox (comments) {
-      var div = $(".comments");
-      $.each(comments, function (i, comment) {
-        div.append("<p>" + comment.text + "</p>");
-      });
-    }
+  function addCommentsToFancybox (comments) {
+    var div = $(".comments");
+    $.each(comments, function (i, comment) {
+      div.append("<p>" + comment.text + "</p>");
+    });
+  }
 
-    function getPictures (latLng) {
-      $.ajax({
-        type: "POST",
-        url: "dashboards/instagram_search",
-        data: instagramFormattedLatLng(latLng),
-        dataType: "json",
-        success: function (data) {
-          dropPins(latLng, data);
-        }
-      });
-    }
+  function getPictures (latLng) {
+    $.ajax({
+      type: "POST",
+      url: "dashboards/instagram_search",
+      data: instagramFormattedLatLng(latLng),
+      dataType: "json",
+      success: function (data) {
+        dropPins(latLng, data);
+      }
+    });
+  }
 
-    function instagramFormattedLatLng (latLng) {
-      return {latitude: latLng.lat(), longitude: latLng.lng()}
-    }
+  function instagramFormattedLatLng (latLng) {
+    return {latitude: latLng.lat(), longitude: latLng.lng()}
+  }
 
-    function dropPins (latLng, data) {
-      map.setCenter(latLng);
+  function dropPins (latLng, data) {
+    map.setCenter(latLng);
 
-      $.each(data, function(i, item) {
-        var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
-        var thumbnail = item.images.thumbnail.url
-        var marker = createMarker(location, thumbnail);
-        addMarkerListener(marker, item);
-      });
-    }
+    $.each(data, function(i, item) {
+      var location = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+      var thumbnail = item.images.thumbnail.url
+      var marker = createMarker(location, thumbnail);
+      googleMap.addMarkerListener(marker, item);
+    });
+  }
 
-    function addMarkerListener (marker, item) {
-      google.maps.event.addListener(marker, 'click', function() {
-        map.panTo(marker.getPosition());
-        $.fancybox({
-          content: fancyboxContent(item),
-          title: captionText(item)
-        });
-        getComments(item.id);
-      });
-    }
+  function fancyboxContent (instagramItem) {
+    return ['<div class="col-md-6">',
+     '<img class="img-responsive"  src=',
+     instagramItem.images.standard_resolution.url,
+     '>',
+    '</div>',
+    '<div class="col-md-6 comments">',
+    captionText(instagramItem),
+    '<br>',
+    '<strong>Comments</strong>',
+    '</div>'].join("\n")
+  }
 
-    function fancyboxContent (instagramItem) {
-      return ['<div class="col-md-6">',
-       '<img class="img-responsive"  src=',
-       instagramItem.images.standard_resolution.url,
-       '>',
-      '</div>',
-      '<div class="col-md-6 comments">',
-      captionText(instagramItem),
-      '<br>',
-      '<strong>Comments</strong>',
-      '</div>'].join("\n")
-    }
+  function captionText (instagramItem) {
+    return instagramItem.caption ? instagramItem.caption.text : ""
+  }
 
-    function captionText (instagramItem) {
-      return instagramItem.caption ? instagramItem.caption.text : ""
-    }
+  function createMarker (latLng, thumbnail) {
+    var images = {
+      url: thumbnail,
+      scaledSize: new google.maps.Size(50, 50)
+    };
+    return new google.maps.Marker({
+      position: latLng,
+      map: map,
+      icon: images,
+      animation: google.maps.Animation.DROP
+    });
+  }
 
-    function createMarker (latLng, thumbnail) {
-      var images = {
-        url: thumbnail,
-        scaledSize: new google.maps.Size(50, 50)
-      };
-      return new google.maps.Marker({
-        position: latLng,
-        map: map,
-        icon: images,
-        animation: google.maps.Animation.DROP
-      });
-    }
-
-  });
 });
